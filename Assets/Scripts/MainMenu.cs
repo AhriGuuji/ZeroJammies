795ea7e5e92ca112d5506a41d51c20d91b49d1da
Daniel <daniel.henriques.gamedev@gmail.com>
2025-11-01 10:12:18 +0000

    Keyboard input on main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
- 
+    [SerializeField]
+    private string _sceneName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(_sceneName);
+    }
 }
