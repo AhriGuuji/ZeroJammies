8f422a3ae597f2ca31ca9a6259835d0043953e35
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-11-02 10:26:10 +0000

    Anomaly sscript

--- Assets/Scripts/ItemBehaviour/Anomaly.cs
+++ Assets/Scripts/ItemBehaviour/Anomaly.cs
@@ -13,21 +13,34 @@ public class Anomaly : Interactable
     private STATE _gameState;
     protected override void Update()
     {
-        Collider2D collider = Physics2D.OverlapCircle(transform.position, _interactRange, _playerLayer);
-        if (!collider) return;
-
-        if (collider.GetComponent<PlayerMovement>())
+        if (_gameState == STATE.Anomaly)
         {
-            if (_interact.WasPressedThisFrame())
+            GetComponent<SpriteRenderer>().sprite = _anomalyState;
+
+            Collider2D collider = Physics2D.OverlapCircle(transform.position, _interactRange, _playerLayer);
+            if (!collider) return;
+
+            if (collider.GetComponent<PlayerMovement>())
             {
+                if (_interact.WasPressedThisFrame())
+                {
+                    _interactionsPressed++;
+                }
+            }
+
+            if (_interactionsPressed == _interactionsNeeded)
+            {
+                GameEvent();
                 _interactionsPressed++;
             }
         }
+    }
 
-        if (_interactionsPressed == _interactionsNeeded)
-        {
-            GameEvent();
-            _interactionsPressed++;
-        }
+    protected override void GameEvent()
+    {
+        _gameState = STATE.Normal;
+        GetComponent<SpriteRenderer>().sprite = _normalState;
+        _interactionsPressed = 0;
     }
 }
+
